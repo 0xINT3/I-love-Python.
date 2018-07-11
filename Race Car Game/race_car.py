aa355769ea155d1f1dd1b61827e7444afda82b42
abhinavs03 <abhimesteryo619@gmail.com>
2018-07-11 12:13:00 +0530

    Racing Car game using PyGame Module

---
+++ Race Car Game/race_car.py
@@ -0,0 +1,80 @@
+import pygame
+import time
+
+display_width = 800
+display_height = 600
+
+black = (0,0,0)         #RGB Colour System (Red-Green-Blue)
+white = (255,255,255)
+#red = (255,0,0)
+
+pygame.init()
+gameDisplay = pygame.display.set_mode((display_width,display_height))
+pygame.display.set_caption('Avoid Hurdles')
+clock = pygame.time.Clock()
+
+carImg = pygame.image.load('racecar.png')
+def car(x,y):
+    gameDisplay.blit(carImg,(x,y))
+
+def text_objects(text,font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+
+def message_display(text):
+    largeText = pygame.font.Font("freesansbold.ttf",60)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = ((display_width/2),(display_height/2))
+    gameDisplay.blit(TextSurf, TextRect)
+
+    pygame.display.update()
+    time.sleep(2)
+    game_loop()
+
+def crash():
+    message_display("OOPS! You Crashed")
+
+def game_loop():
+    x = (display_width * 0.45)
+    y = (display_height * 0.8)
+    x_change = 0
+    y_change = 0
+
+    gameExit = False
+
+    while not gameExit:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+            if event.type == pygame.KEYDOWN:
+
+                    if event.key == pygame.K_LEFT:
+                        x_change = -5
+                    if event.key == pygame.K_UP:
+                        y_change = -5
+                    if event.key == pygame.K_DOWN:
+                        y_change = 5
+                    if event.key == pygame.K_RIGHT:
+                        x_change = 5
+
+            if event.type ==pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    x_change = 0
+                    y_change = 0
+
+        x += x_change
+        y += y_change
+
+        gameDisplay.fill(white)
+        car(x,y)
+
+        if x == display_width - 80 or x < 0:
+            crash()
+
+        pygame.display.update()
+        clock.tick(60)
+game_loop()
+pygame.quit()
+quit()
